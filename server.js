05c22a33af3a57cd3746a4ccb2a7cff99d2a91c4
Kenny <kennymais@gmail.com>
2020-04-18 18:36:33 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var AccessToken = require("twilio").jwt.AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
+var port = process.env.PORT || 5000;
+
+var ACCOUNT_SID = "ACb117e5ba1426d34171d9b66bffa4f98c";
+var API_KEY_SID = "SK7648e3c0a1a89553b51795101b52e4d6";
+var API_KEY_SECRET = "edYdQeRZYZPb1gVyIuMHLHvBTJ8GmY35";
+
+var accessToken = new AccessToken(ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET);
+
+// Set the Identity of this token
+accessToken.identity = "example-user";
+
+// Grant access to Video
+var grant = new VideoGrant();
+grant.room = "cool room";
+accessToken.addGrant(grant);
+
+// Serialize the token as a JWT
+/*
+var jwt = accessToken.toJwt();
+console.log(jwt);
+*/
+
+app.get("/jwt", function (req, res) {
+  var jwt = accessToken.toJwt();
+  //console.log(jwt);
+  res.send(jwt);
+});
+
+app.listen(port, function () {
+  console.log("Server up and Listening");
+});
