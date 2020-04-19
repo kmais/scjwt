3e7d1ae704a999e1ca87320a9f6259cf6e0ef69a
Kenny <kennymais@gmail.com>
2020-04-19 12:20:54 -0500

    commited

--- server.js
+++ server.js
@@ -8,25 +8,31 @@ var ACCOUNT_SID = "ACb117e5ba1426d34171d9b66bffa4f98c";
 var API_KEY_SID = "SK7648e3c0a1a89553b51795101b52e4d6";
 var API_KEY_SECRET = "edYdQeRZYZPb1gVyIuMHLHvBTJ8GmY35";
 
-var accessToken = new AccessToken(ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET);
-
-// Set the Identity of this token
-accessToken.identity = "example-user";
-
-// Grant access to Video
-var grant = new VideoGrant();
-grant.room = "cool room";
-accessToken.addGrant(grant);
-console.log("hello");
-
 // Serialize the token as a JWT
 /*
-var jwt = accessToken.toJwt();
-console.log(jwt);
+
+var ACCOUNT_SID = "ACb117e5ba1426d34171d9b66bffa4f98c";
+var API_KEY_SID = "SK7648e3c0a1a89553b51795101b52e4d6";
+var API_KEY_SECRET = "edYdQeRZYZPb1gVyIuMHLHvBTJ8GmY35";
+
 */
 
-app.get("/jwt/:ss/:room/:name", function (req, res) {
+app.get("/jwt/:id/:room", function (req, res) {
+  console.log("hello");
   console.log(req.params);
+
+  var accessToken = new AccessToken(ACCOUNT_SID, API_KEY_SID, req.params.id);
+
+  console.log(accessToken);
+
+  // Set the Identity of this token
+  accessToken.identity = "example-user";
+
+  // Grant access to Video
+  var grant = new VideoGrant();
+  grant.room = req.params.room;
+  accessToken.addGrant(grant);
+  console.log(accessToken);
   var jwt = accessToken.toJwt();
   //console.log(jwt);
   res.send(jwt);
